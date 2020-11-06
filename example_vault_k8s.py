30606a7278170dee985e6521685e66febf849a58
eshaingle <73927429+eshaingle@users.noreply.github.com>
2020-11-06 08:40:26 +0100

    Update example_vault_k8s.py

--- example_vault_k8s.py
+++ example_vault_k8s.py
@@ -9,7 +9,7 @@ from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOpera
 from airflow.hooks.base_hook import BaseHook
 from airflow.providers.hashicorp.secrets.vault import VaultBackend
 
-os.environ['AIRFLOW__SECRETS__BACKEND'] = "airflow.providers.hashicorp.secrets.vault.VaultBacken"
+os.environ['AIRFLOW__SECRETS__BACKEND'] = "airflow.providers.hashicorp.secrets.vault.VaultBackend"
 os.environ['AIRFLOW__SECRETS__BACKEND_KWARGS'] = '{"connections_path": "myapp", "auth_mount_point": "kubernetes", "mount_point": "secret", "auth_type": "kubernetes", "kubernetes_role": "example", "kubernetes_jwt_path":"/var/run/secrets/kubernetes.io/serviceaccount/token", "url": "http://192.168.49.1:8200"}'
 
 def get_secrets(**kwargs):
