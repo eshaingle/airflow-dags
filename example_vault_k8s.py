8ec91f580e75fb66dd1878976a6ec9e2ed81b5b8
eshaingle <73927429+eshaingle@users.noreply.github.com>
2020-11-06 09:24:47 +0100

    Update example_vault_k8s.py

--- example_vault_k8s.py
+++ example_vault_k8s.py
@@ -33,7 +33,7 @@ passing = KubernetesPodOperator(namespace='default',
                                 image="alpine:3.7",
                                 cmds=["sh", "-cx"],
                                 arguments=["apk add curl && curl --request POST \
-        	--data '{"'"jwt"'": "'"eyJhbGciOiJSUzI1NiIsImtpZCI6Imh3T0U2QXNSVjVHUzlySG0tVDh5U0k5eVNSeEJWLUN0SzJYcFVrVEtYek0ifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJkZWZhdWx0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6InZhdWx0LWF1dGgtdG9rZW4tODJmY2YiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoidmF1bHQtYXV0aCIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6ImIxMWU1ZDdjLTMxZjQtNDFlMi1hM2MzLWY1MDI4ZWRhZWE4NCIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDpkZWZhdWx0OnZhdWx0LWF1dGgifQ.TIO1gsY47wdsJVCksdFFlypsJ4SY0vSCmnUWkoqNseI4zlp3tR5y1xnSko_6gM9kF8tenfVvhf-oTkSecaFFzmVTlhA4YJopMAHUH7bdXM_UgrFFyv2hKx6ZwZrW6H5T-GZ0gsyUbxfsxqFc12-Qvh5mNN5g7XcgFbBdWcdwVUucmhwIVSf5xH3hpQljus1R4x0lOd-xvGqZwB6NEFpj6CepA1kLWVh6V_YxdSxTDzdBlPoaG04a8WNybvn0rvlUjQ6pNGPv0uEDpnKNgiL8zJkWr3FaAId7gvPALhHjGIWpmp1VIfqmchscKqZZBEaqhKKs06pmWhWZTRA9DK19UA"'", "'"role"'": "'"example"'"}' \
+        	--data '{"'"jwt"'": "'"$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"'", "'"role"'": "'"example"'"}' \
         	http://192.168.49.1:8200/v1/auth/kubernetes/login "],
                                 labels={"test-airflow": "firstversion"},
                                 name="passing-test",
