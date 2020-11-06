3b4b249454a4d3375702d6fea9afc0e8d38a9e97
eshaingle <73927429+eshaingle@users.noreply.github.com>
2020-11-06 10:12:34 +0100

    Update example_vault.py

--- example_vault.py
+++ example_vault.py
@@ -3,13 +3,14 @@ from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
 from airflow.hooks.base_hook import BaseHook
 
+os.environ['AIRFLOW__SECRETS__BACKEND'] = "airflow.contrib.secrets.hashicorp_vault.VaultBackend"
+os.environ['AIRFLOW__SECRETS__BACKEND_KWARGS'] = '{"connections_path": "myapp", "mount_point": "secret", "auth_type": "token", "token": "s.BnK30oKUMgkNZ7UkvnqBi4Je", "url": "http://127.0.0.1:8200"}'
 
 def get_secrets(**kwargs):
     conn = BaseHook.get_connection(kwargs['my_conn_id'])
     print("Password:", {conn.password} )
-    print(" Login:", {conn.login} )
-    print(" URI:", {conn.get_uri()} )
-    print("Host:", {conn.host})
+    print("Username:", {conn.username} )
+    print("TTL:", {conn.ttl} )
 
 with DAG('vault_example', start_date=datetime(2020, 1, 1), schedule_interval=None) as dag:
 
@@ -17,5 +18,5 @@ with DAG('vault_example', start_date=datetime(2020, 1, 1), schedule_interval=Non
     test_task = PythonOperator(
         task_id='test-task',
         python_callable=get_secrets,
-        op_kwargs={'my_conn_id': 'smtp_default'},
+        op_kwargs={'my_conn_id': 'config'},
     )
