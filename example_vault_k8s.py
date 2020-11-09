b3af174a13c7c0dbb572c870ac458603a39af7d6
eshaingle <73927429+eshaingle@users.noreply.github.com>
2020-11-09 13:28:55 +0100

    Update example_vault_k8s.py

--- example_vault_k8s.py
+++ example_vault_k8s.py
@@ -10,13 +10,14 @@ from airflow.hooks.base_hook import BaseHook
 from airflow.providers.hashicorp.secrets.vault import VaultBackend
 
 os.environ['AIRFLOW__SECRETS__BACKEND'] = "airflow.providers.hashicorp.secrets.vault.VaultBackend"
-os.environ['AIRFLOW__SECRETS__BACKEND_KWARGS'] = '{"connections_path": "myapp", "auth_mount_point": "kubernetes", "mount_point": "secret", "auth_type": "kubernetes", "kubernetes_role": "example", "kubernetes_jwt_path":"/var/run/secrets/kubernetes.io/serviceaccount/token", "url": "http://192.168.49.1:8200"}'
+os.environ['AIRFLOW__SECRETS__BACKEND_KWARGS'] = '{"connections_path": "message", "auth_mount_point": "kubernetes", "mount_point": "kv", "auth_type": "kubernetes", "kubernetes_role": "example", "kubernetes_jwt_path":"/var/run/secrets/kubernetes.io/serviceaccount/token", "url": "http://vault.default.svc:8200"}'
 
 def get_secrets(**kwargs):
     conn = BaseHook.get_connection(kwargs['my_conn_id'])
     print("Password:", {conn.password} )
-    print(" Login:", {conn.username} )
-    print("Url:", {environ.get("CLIENT_TOKEN")})
+    print(" Login:", {conn.login} )
+    print(" URI:", {conn.get_uri()} )
+    print("Host:", {conn.host})
 
 
 dag = DAG(
