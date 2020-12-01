806d9b555f8ce106773439c1dd6e32bfd24dc10b
eshaingle <73927429+eshaingle@users.noreply.github.com>
2020-12-01 12:09:50 +0100

    Update example_vault.py

--- example_vault.py
+++ example_vault.py
@@ -9,7 +9,7 @@ os.environ['AIRFLOW__SECRETS__BACKEND'] = "airflow.providers.hashicorp.secrets.v
 os.environ['AIRFLOW__SECRETS__BACKEND_KWARGS'] = '{"connections_path": "myapp", "mount_point": "secret", "auth_type": "token", "token": "s.C6wS8kvj8nc3IH7qEtnBXkTa", "url": "http://localhost:8200"}'
 
 def get_secrets(**kwargs):
-    conn = BaseHook.get_connection(kwargs['config'])
+    conn = BaseHook.get_connection(kwargs['my_conn_id'])
     print("Password:", {conn.password} )
     print(" Login:", {conn.username} )
 
@@ -19,5 +19,5 @@ with DAG('vault_example', start_date=datetime(2020, 1, 1), schedule_interval=Non
     test_task = PythonOperator(
         task_id='test-task',
         python_callable=get_secrets,
-        op_kwargs={'my_conn_id': 'smtp_default'},
+        op_kwargs={'my_conn_id': 'config'},
     )
